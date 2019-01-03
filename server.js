9ef5561ddfeda802d8ab1da0f5091348deb43cdb
Ilian Ivanov <noodle.hell@gmail.com>
2019-01-03 12:49:09 +0200

    Revert "first commit"
    This reverts commit 76ca649daeeb0d55b46459c9d3123ecfd088e834.
    
    	deleted:    Front-end-React/node_modules/postcss-selector-not/node_modules/postcss/docs/architecture.md

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const port = 3001;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 app.get('/', function (req, res) {
     res.send("HELLO");
 });
 
 io.on('connection', function (socket) {
-    var clients = io.of('/').clients()
-    console.log(clients)
         console.log("NEW USER")
     socket.on('disconnect', function () {
         console.log('user disconnected');
